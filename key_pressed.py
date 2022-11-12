9bd1490aab9528c970d2d0e720274e5719a8822b
Om sai prakash reddy <104917716+prakashreddy5155@users.noreply.github.com>
2022-11-12 20:15:34 +0530

    Add files via upload

---
+++ key_pressed.py
@@ -0,0 +1,93 @@
+from pygame import mixer 
+import string
+
+mixer.init()
+# letters = string.ascii_letters
+# print(letters)
+import keyboard
+while True:
+    if keyboard.is_pressed('a'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play() 
+    if keyboard.is_pressed('b'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('c'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('d'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('e'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('f'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('g'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('h'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('i'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('j'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('k'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('l'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('m'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('n'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('o'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('p'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('q'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('r'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('s'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('t'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('u'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('v'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('w'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('x'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('y'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed('z'):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+    if keyboard.is_pressed(' '):
+        mixer.music.load("./keypress.mp3")
+        mixer.music.play()
+        
+    
+        
+        
